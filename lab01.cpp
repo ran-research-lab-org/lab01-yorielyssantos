7c8c62c1673ee1eff3f5bcdb374fcfc1d24a34c7
yorielyssantos <yorielys.delossantos@upr.edu>
2024-08-23 16:55:31 +0000

    Part 3: Write code to submit

---
+++ lab01.cpp
@@ -0,0 +1,20 @@
+// (Your name and github username)
+// A program to practice modifications and compilation
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!"        << std::endl;
+  std::cout << "Hello Solar System!" << std::endl;
+  std::cout << "Hello Galaxy!"       << std::endl;
+  std::cout << "Hello Universe!"     << std::endl;
+  std::cout << "Hello ... ?!"        << std::endl;
+  std::cout << "This is a 1!"        << std::endl;
+  std::cout << "This is a 2!"        << std::endl;
+  std::cout << "This is a 3!"        << std::endl;
+  std::cout << "This is a 4!"        << std::endl;
+  std::cout << "This is a 5!"        << std::endl;
+  
+  return 0;
+}
+
