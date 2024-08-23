7b1daedb15d760d7dce3f208469cb7d750ce64a9
yorielyssantos <yorielys.delossantos@upr.edu>
2024-08-23 16:19:21 +0000

    ccom

---
+++ add.cpp
@@ -0,0 +1,14 @@
+// (Your name and github username)
+// This program adds two numbers
+
+#include <iostream>
+
+int main() {
+  int number1, number2, sum;
+  number1 = 12;
+  number2 = 13;
+  sum = number1 + number2;
+  std::cout << "The sum of these two integers is "
+            << sum << std::endl;
+  return 0;
+}
